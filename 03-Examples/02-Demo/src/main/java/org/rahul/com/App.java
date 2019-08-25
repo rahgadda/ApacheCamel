53236ff1364dc7457189e178dedf84989cd4c49e
unknown <gaddam.rahul@gmail.com>
2019-08-25 21:10:31 +0530

    Initia Draft

--- 03-Examples/02-Demo/src/main/java/org/rahul/com/App.java
+++ 03-Examples/02-Demo/src/main/java/org/rahul/com/App.java
@@ -1,9 +1,14 @@
 package org.rahul.com;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
 public class App {
     public static void main(String args[]) throws Exception {
         System.out.println("Starting Apache Camel Rest Consume Demo!");
-        
+        CamelContext camel = new DefaultCamelContext();
+        camel.addRoutes(new Routes());
+        camel.start();
         System.out.println("Ending Apache Camel Rest Consume Demo!");
     }
 }
--- 03-Examples/02-Demo/src/main/java/org/rahul/com/Routes.java
+++ 03-Examples/02-Demo/src/main/java/org/rahul/com/Routes.java
@@ -5,6 +5,7 @@ import org.apache.camel.builder.RouteBuilder;
 public class Routes extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("servlet:https://apex.oracle.com/pls/apex/oraclejet/emp/")
+        from("servlet://https:////apex.oracle.com//pls//apex//oraclejet//emp").transform().simple("{$in.body}")
+                .to("stream:out");
     }
 }
