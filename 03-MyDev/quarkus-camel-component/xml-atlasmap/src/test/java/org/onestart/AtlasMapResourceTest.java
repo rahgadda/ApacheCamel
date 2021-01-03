c28404a2b5a179d332b089d9fdf30b25f12d0028
Rahul Kiran Gaddam <gaddam.rahul@gmail.com>
2021-01-03 15:25:30 +0530

    Initial Draft

---
+++ 03-MyDev/quarkus-camel-component/xml-atlasmap/src/test/java/org/onestart/AtlasMapResourceTest.java
@@ -0,0 +1,21 @@
+package org.onestart;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class AtlasMapResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
---
+++ 03-MyDev/quarkus-camel-component/xml-atlasmap/src/test/java/org/onestart/NativeAtlasMapResourceIT.java
@@ -0,0 +1,9 @@
+package org.onestart;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAtlasMapResourceIT extends AtlasMapResourceTest {
+
+    // Execute the same tests but in native mode.
+}
