bd24adf8f9e6aa16fd638e3a7ea7da5ee6936a7d
Rahul Kiran Gaddam <gaddam.rahul@gmail.com>
2021-01-03 15:30:41 +0530

    Initial Draft

---
+++ 03-MyDev/quarkus-no-camel/atlasmap/json-to-json/transformation/src/main/java/org/onestart/TransformationResource.java
@@ -0,0 +1,65 @@
+package org.onestart;
+
+import java.io.File;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import io.atlasmap.api.AtlasContext;
+import io.atlasmap.api.AtlasContextFactory;
+import io.atlasmap.api.AtlasSession;
+import io.atlasmap.core.DefaultAtlasContextFactory;
+
+@Path("/hello")
+public class TransformationResource {
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String transform(String source) throws Exception {
+        // Artifacts
+        
+        //simple
+        //String admLocation = "D:\\02-MyDev\\12-Camel\\02-MyDev\\10-Atlasmap-dev1\\transformation\\src\\main\\resources\\mapping\\simple-UI.0.json";
+
+        //comment
+        String admLocation = "D:\\02-MyDev\\12-Camel\\02-MyDev\\10-Atlasmap-dev1\\transformation\\src\\main\\resources\\mapping\\comment-UI.0.json";
+
+        // Creating Atlasmap Session
+        AtlasContextFactory factory = DefaultAtlasContextFactory.getInstance();
+        AtlasContext context = factory.createContext(new File(admLocation));
+        AtlasSession session = context.createSession();
+
+        // Setting Source
+        session.setSourceDocument("input_comment", source);
+
+        // Transformation
+        context.process(session);
+
+        // Generating Destination
+        String targetDoc = (String) session.getTargetDocument("output_comment");
+        System.out.println("Source value is " + session.getSourceDocument("input_comment"));
+        System.out.println("Transformed value is " + targetDoc);
+        return targetDoc;
+        
+        // String source = Files.readString(Paths.get(inputFile));
+        // System.out.println("Source document:\n" + source);
+        // context.processValidation(session);
+        // System.out.println(session.getValidations().toString());
+        // if(session.getValidations() == null){
+        // System.out.println("No Validations");
+        // context.process(session);
+        // }else{
+        // System.out.println("Has Validations of
+        // "+session.getValidations().getValidation().size());
+        // Validations validations = session.getValidations();
+        // for(Validation validation : validations.getValidation()){
+        // System.out.println(" Validations "+validation.getMessage());
+        // }
+        // }
+        // String inputFile =
+        // "D:\\02-MyDev\\12-Camel\\02-MyDev\\10-Atlasmap-dev1\\transformation\\src\\main\\resources\\data\\input.json";
+
+    }
+}
---
+++ 03-MyDev/quarkus-no-camel/atlasmap/json-to-json/transformation/src/test/java/org/onestart/NativeTransformationResourceIT.java
@@ -0,0 +1,9 @@
+package org.onestart;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTransformationResourceIT extends TransformationResourceTest {
+
+    // Execute the same tests but in native mode.
+}
